6beb6bc06261506d8af729d8268baf52f6cfe3f9
Aaron <heyitsronex@gmail.com>
2018-03-12 23:38:02 -0400

    sync animation stroke with timer; changed properties' names

--- SpinWorkout/WorkoutViewController.swift
+++ SpinWorkout/WorkoutViewController.swift
@@ -25,19 +25,27 @@ class WorkoutViewController: UIViewController {
     var timerPause: Bool = false
     var timeInterval: TimeInterval = 0.1
     
-    var maxWorkoutTime: TimeInterval = 0.0
-    var maxSetTime: TimeInterval = 0.0
-    var workoutTimeCount: TimeInterval = 0.0
-    var setTimeCount: TimeInterval = 0.0
+    var currentTimeElapsed : TimeInterval = 0
+    var lastStartTime : Date? = nil
+    
+    var paused = true
+    var firstTime = false
+    
+    var totalTime: TimeInterval = 0.0 // total of everything
+    var currentSetTotalTime: TimeInterval = 0.0 // total duration of each as its currently running
+    var totalTimeRemaining: TimeInterval = 0.0 // total of everything remaining
+    var setTimeCount: TimeInterval = 0.0 // ??
+    
+    var ratioRemainingTime: TimeInterval?
     
     var workout: SpinWorkout?
     
-    var setNumber: Int = 0
+    var setIndex: Int = 0
     var setCount: Int = 0
     
     var gradientLayer: CAGradientLayer!
     let shapeLayer = CAShapeLayer()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -55,50 +63,102 @@ class WorkoutViewController: UIViewController {
             workoutSeconds += seconds
         }
         
-        maxWorkoutTime = workoutSeconds
+        totalTime = workoutSeconds
         
         resetWorkout()
         nextWorkoutSet()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
     // MARK: Private Methods
     
-    @objc private func handleTap() {
     
-            ///////// start stroke animation
+    
+    @objc private func handleTap() {
+        
+        if paused == true {
+            // start/resume
+            if firstTime == false {
+            perform(#selector(startTimer), with: nil, afterDelay: 0.0)
+            startAnimation()
+            
+            } else {
+                perform(#selector(startTimer), with: nil, afterDelay: 0.0)
+                resumeAnimation()
+            }
+            lastStartTime = Date()
+            
+        } else {
+            if let lastStartTime = lastStartTime {
+                self.currentTimeElapsed += Date().timeIntervalSince(lastStartTime)
+                self.lastStartTime = nil
+            }
+            
+            pauseTimer()
+            pauseAnimation()
+        }
+        paused = !paused
+    }
+    
+    private func pauseTimer() {
+        stopTimer()
+        pauseAnimation()
+    }
+    
+    private func pauseAnimation() {
+        
+        ratioRemainingTime = percentElapsedTime()
+        
+        // remove animation
+        
+        shapeLayer.removeAllAnimations()
+        
+        // stroke the circle
+        if let ratioRemainingTime = ratioRemainingTime {
+            
+            let center = circleContainerView.center
+            let circularPath = UIBezierPath(arcCenter: center, radius: 145, startAngle: -90.degreesToRadians, endAngle: (-90+360).degreesToRadians, clockwise: true)
+            shapeLayer.path = circularPath.cgPath
+            shapeLayer.strokeColor = UIColor(red: 214.0/255.0, green: 150.0/255.0, blue: 56.0/255.0, alpha: 1.0).cgColor
+            shapeLayer.lineWidth = 10
+            shapeLayer.lineCap = kCALineCapRound
+            shapeLayer.strokeEnd = CGFloat(1.0 - ratioRemainingTime)
+            print("ratioRemainingTime: \(1 - ratioRemainingTime)")
+            
+        }
+    }
+    
+    private func resumeAnimation() {
+        
+        if let ratioRemainingTime = ratioRemainingTime {
             let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-            basicAnimation.fromValue = 0
+            basicAnimation.fromValue = 1 - ratioRemainingTime
             basicAnimation.toValue = 1
-            basicAnimation.duration = maxWorkoutTime
-            print(maxWorkoutTime)
+            basicAnimation.duration = totalTimeRemaining
             basicAnimation.fillMode = kCAFillModeForwards
             basicAnimation.isRemovedOnCompletion = false
             
             shapeLayer.add(basicAnimation, forKey: "strokeAnimation")
-        
-        /////////
-        
-        timerPause = !timerPause
-        
-        if timerPause == true {
-            startTimer()
-        } else {
-            stopTimer()
         }
-    
     }
-
+    
+    private func startAnimation() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.fromValue = 0
+        basicAnimation.toValue = 1
+        basicAnimation.duration = totalTime
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "strokeAnimation")
+    }
+    
     private func createRingLayer() {
         
         let center = circleContainerView.center
-
+        
         let trackLayer = CAShapeLayer()
-        let circularPath = UIBezierPath(arcCenter: center, radius: 145, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: center, radius: 145, startAngle: -90.degreesToRadians, endAngle: (-90+360).degreesToRadians, clockwise: true)
         
         trackLayer.path = circularPath.cgPath
         trackLayer.strokeColor = UIColor(red: 214.0/255.0, green: 150.0/255.0, blue: 56.0/255.0, alpha: 0.25).cgColor
@@ -113,7 +173,7 @@ class WorkoutViewController: UIViewController {
         shapeLayer.lineWidth = 10
         shapeLayer.lineCap = kCALineCapRound
         shapeLayer.strokeEnd = 0
-    
+        
         view.layer.addSublayer(shapeLayer)
     }
     
@@ -130,126 +190,75 @@ class WorkoutViewController: UIViewController {
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
+    @objc private func startTimer() {
+        firstTime = true
+        timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: (#selector(self.timerDidEnd)), userInfo: nil, repeats: true)
+    }
+    
+    private func stopTimer() {
+        timer.invalidate()
+    }
+    
     
     //MARK: Workouts
     
-    @IBAction func startButtonTapped(_ sender: UIButton) {
-
-//        /////// start stroke animation
-//        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-//        basicAnimation.fromValue = 0
-//        basicAnimation.toValue = 1
-//        basicAnimation.duration = maxWorkoutTime
-//        basicAnimation.fillMode = kCAFillModeForwards
-//        basicAnimation.isRemovedOnCompletion = false
-//
-//        shapeLayer.add(basicAnimation, forKey: "strokeAnimation")
-//        ///////
-//
-//        var titleLabel = ""
-//
-//        if sender.titleLabel?.text == "Start" || sender.titleLabel?.text == "Resume"{
-//            startTimer()
-//            if timerPause {
-//                timerPause = false
-//
-//            } else {
-//                nextWorkoutSet()
-//
-//            }
-//
-//            titleLabel = "Pause"
-//        } else {
-//            stopTimer()
-//
-//            timerPause = true
-//
-//            titleLabel = "Resume"
-//        }
-//
-//        sender.setTitle(titleLabel, for: .normal)
-//        sender.setTitle(titleLabel, for: .selected)
-//        sender.setTitle(titleLabel, for: .highlighted)
-//
-//        sender.sizeToFit()
-
-    }
-    
     func nextWorkoutSet() {
         
-        if setNumber >= setCount {
+        if setIndex >= setCount {
             return
         }
         
-        let gear = workout?.sets![setNumber].gear ?? 0
-        let cadence = workout?.sets![setNumber].cadence ?? 0
-        let seconds = workout?.sets![setNumber].seconds ?? 0.0
+        let gear = workout?.sets![setIndex].gear ?? 0
+        let cadence = workout?.sets![setIndex].cadence ?? 0
+        let seconds = workout?.sets![setIndex].seconds ?? 0.0
         
         
-        if (setNumber + 1) < setCount {
+        if (setIndex + 1) < setCount {
             
-            let nextGear = workout?.sets![setNumber + 1 ].gear ?? 0
-            let nextCadence = workout?.sets![setNumber + 1].cadence ?? 0
+            let nextGear = workout?.sets![setIndex + 1 ].gear ?? 0
+            let nextCadence = workout?.sets![setIndex + 1].cadence ?? 0
             
             nextGearLabel.text = "\(nextGear)"
             nextCadenceLabel.text = "\(nextCadence)"
-    
+            
         } else {
             nextGearLabel.text = " - "
             nextCadenceLabel.text = " --"
         }
-
-        maxSetTime = seconds
+        
+        currentSetTotalTime = seconds
         
         gearLabel.text = "\(gear)"
         gearLabel.sizeToFit()
         
         cadenceLabel.text = "\(cadence)"
         cadenceLabel.sizeToFit()
-       
-        currentSetLabel.text = "SET \(setNumber + 1) / \(setCount)"
+        
+        currentSetLabel.text = "SET \(setIndex + 1) / \(setCount)"
         
         if timerCountDown {
-            setTimeCount = maxSetTime
+            setTimeCount = currentSetTotalTime
         } else {
             setTimeCount = 0.0
         }
         
-        if setNumber == 0 {
-            workoutTimerLabel.text = timeString(interval: workoutTimeCount, format: "hm")
+        if setIndex == 0 {
+            workoutTimerLabel.text = timeString(interval: totalTimeRemaining, format: "hm")
             setTimerLabel.text = timeString(interval: setTimeCount, format: "hms")
         }
         
     }
     
     func resetWorkout() {
-        setNumber = 0
+        setIndex = 0
         
         if timerCountDown {
-            workoutTimeCount = maxWorkoutTime
+            totalTimeRemaining = totalTime
         } else {
-            workoutTimeCount = 0.0
+            totalTimeRemaining = 0.0
         }
         
         timerPause = false
-        
-        //        let titleLabel = "Start"
-        //
-        //        startButtonLabel.setTitle(titleLabel, for: .normal)
-        //        startButtonLabel.setTitle(titleLabel, for: .selected)
-        //        startButtonLabel.setTitle(titleLabel, for: .highlighted)
-    }
-
-    func startTimer() {
-        // prevent more than one timer on the thread
-        if !timer.isValid {
-            timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: (#selector(self.timerDidEnd)), userInfo: nil, repeats: true)
-        }
-        
-    }
-    
-    func stopTimer() {
-        timer.invalidate()
     }
     
     func timeString(interval: TimeInterval, format: String) -> String {
@@ -266,54 +275,57 @@ class WorkoutViewController: UIViewController {
         }
     }
     
+    private func percentElapsedTime()-> TimeInterval {
+    
+            totalTimeRemaining = (totalTime - currentTimeElapsed)
+            return totalTimeRemaining / totalTime
+    }
+    
     @objc func timerDidEnd(timer: Timer) {
-        if timerCountDown {
-            // timer that counts down
-            workoutTimeCount = workoutTimeCount - timeInterval
-            setTimeCount = setTimeCount - timeInterval
-            if setTimeCount <= 0 {
-                if workoutTimeCount <= 0 {
-                    timer.invalidate()
-                    resetWorkout()
-                } else {
-                    setNumber += 1
-                    nextWorkoutSet()
-                }
+        
+        // timer that counts down
+        totalTimeRemaining = totalTimeRemaining - timeInterval
+        print(totalTimeRemaining)
+        
+        setTimeCount = setTimeCount - timeInterval
+        
+        if setTimeCount <= 0 {
+            if totalTimeRemaining <= 0 {
+                timer.invalidate()
+                resetWorkout()
             } else {
-                workoutTimerLabel.text = timeString(interval: workoutTimeCount, format: "hm")
-                setTimerLabel.text = timeString(interval: setTimeCount, format: "hms")
-//                let timer1 = timeString(interval: workoutTimeCount, format: "hms")
-//                let timer2 = timeString(interval: setTimeCount, format: "hms")
-//                if timer1 != workoutTimerLabel.text && timer2 != setTimerLabel.text {
-//                    workoutTimerLabel.text = timer1
-//                    setTimerLabel.text = timer2
-//                }
-
+                setIndex += 1
+                nextWorkoutSet()
             }
         } else {
-            // timer that counts up
-            workoutTimeCount = workoutTimeCount + timeInterval
-            setTimeCount = setTimeCount + timeInterval
-            if setTimeCount >= maxSetTime {
-                if workoutTimeCount >= maxWorkoutTime {
-                    timer.invalidate()
-                    resetWorkout()
-                } else {
-                    setNumber += 1
-                    nextWorkoutSet()
-                }
-            } else {
-                workoutTimerLabel.text = timeString(interval: workoutTimeCount, format: "hm")
-                setTimerLabel.text = timeString(interval: setTimeCount, format: "hms")
-//                let timer1 = timeString(interval: workoutTimeCount, format: "hms")
-//                let timer2 = timeString(interval: setTimeCount, format: "hms")
-//                if timer1 != workoutTimerLabel.text && timer2 != setTimerLabel.text {
-//                    workoutTimerLabel.text = timer1
-//                    setTimerLabel.text = timer2
-//                }
-
-            }
+            workoutTimerLabel.text = timeString(interval: totalTimeRemaining, format: "hm")
+            setTimerLabel.text = timeString(interval: setTimeCount, format: "hms")
+            
         }
+        
+        // setTimeCount > 0
+        //        else {
+        //            // timer that counts up
+        //            totalTimeRemaining = totalTimeRemaining + timeInterval
+        //            setTimeCount = setTimeCount + timeInterval
+        //            if setTimeCount >= currentSetTotalTime {
+        //                if totalTimeRemaining >= totalTime {
+        //                    timer.invalidate()
+        //                    resetWorkout()
+        //                } else {
+        //                    setIndex += 1
+        //                    nextWorkoutSet()
+        //                }
+        //            } else {
+        //                workoutTimerLabel.text = timeString(interval: totalTimeRemaining, format: "hm")
+        //                setTimerLabel.text = timeString(interval: setTimeCount, format: "hms")
+        //
+        //            }
+        //        }
     }
+    
+}
 
+extension BinaryInteger {
+    var degreesToRadians: CGFloat { return CGFloat(Int(self)) * .pi / 180 }
 }
